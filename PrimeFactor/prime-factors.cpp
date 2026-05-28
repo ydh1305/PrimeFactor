bfdf835cc1ecd7fe52904f5380a95cd7cf54afea
donghyun.yoon <ydh1305@naver.com>
2026-05-28 09:07:48 +0900

    [refactoring] page #35

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -8,11 +8,7 @@ public:
 		vector<int> result = {};
 		if (number > 1) {
 			if (number == 4) {
-				if (number % 2 == 0) {
-					result.push_back(2);
-					number /= 2;
-				}
-				if (number % 2 == 0) {
+				while (number % 2 == 0) {
 					result.push_back(2);
 					number /= 2;
 				}
